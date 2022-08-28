93511abf6d01747fdeaf3e85ac26f7a79b48c7ef
ravividap <ravividap@ymail.com>
2022-08-28 14:30:00 +0530

    dapr eg

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Dapr.Client;
+
+const string storeName = "statestore";
+const string key = "counter";
+
+var daprClinet = new DaprClientBuilder().Build();
+var counter = await daprClinet.GetStateAsync<int>(storeName,key);
+
+while(true){
+    Console.WriteLine($"Counter {counter++}");
+    await daprClinet.SaveStateAsync(storeName,key,counter);
+    await Task.Delay(1000);
+}
